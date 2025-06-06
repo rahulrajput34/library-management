51b1f2a8ebae1f0446bae6016ce105b18a697c66
Rahul Rajput <rajputrahul3424@gmail.com>
2025-06-05 23:33:04 -0400

    Refactor BookList component to accept props for title and books, improving reusability

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import BookCard from "./BookCard";
 
-const BookList = () => {
+interface Props {
+  title: string;
+  books: Book[];
+  containerClassName?: string;
+}
+
+const BookList = ({ title, books, containerClassName }: Props) => {
+  if (books.length < 2) return null;
+
   return (
-    <section>
-      <h2 className="font-bebas-neue text-4xl text-gray-100">
-        Popular Books
+    <section className={containerClassName}>
+      <h2 className="font-bebas-neue text-3xl md:text-4xl text-light-100">
+        {title}
       </h2>
+
+      <ul className="mt-6 flex flex-nowrap gap-8 md:gap-12 overflow-x-auto pb-2 scrollbar-hide">
+        {books.map((book) => (
+          <BookCard key={book.id ?? book.title} {...book} />
+        ))}
+      </ul>
     </section>
   );
 };
