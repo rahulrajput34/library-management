6304142b2733c9141db26a78f05f5898fc354aa7
Rahul Rajput <rajputrahul3424@gmail.com>
2025-06-10 23:02:40 -0400

    Add Toaster component to RootLayout for improved user notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
 import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/sonner";
 
 // Importing local fonts
 const ibmPlexSans = localFont({
@@ -36,6 +37,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
