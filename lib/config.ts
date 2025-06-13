39a60af5f243894391d48ce91b9184b7f2dee453
Rahul Rajput <rajputrahul3424@gmail.com>
2025-06-12 20:36:09 -0400

    fix: update privateKey environment variable to use NEXT_PUBLIC prefix

--- lib/config.ts
+++ lib/config.ts
@@ -4,7 +4,7 @@ const config = {
     imagekit: {
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL!,
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
-      privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+      privateKey: process.env.NEXT_PUBLIC_IMAGEKIT_PRIVATE_KEY!,
     },
     databaseUrl: process.env.DATABASE_URL!,
   },
