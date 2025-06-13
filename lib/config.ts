922c5eb2809452f2635c8f35b5fb82d2024cbe72
Rahul Rajput <rajputrahul3424@gmail.com>
2025-06-12 21:19:08 -0400

    fix: update databaseUrl environment variable to use NEXT_PUBLIC prefix

--- lib/config.ts
+++ lib/config.ts
@@ -6,7 +6,7 @@ const config = {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       privateKey: process.env.NEXT_PUBLIC_IMAGEKIT_PRIVATE_KEY!,
     },
-    databaseUrl: process.env.DATABASE_URL!,
+    databaseUrl: process.env.NEXT_PUBLIC_DATABASE_URL!,
   },
 };
 
