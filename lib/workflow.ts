10b1de2b6ad79f01ce565e60d307d820979e5785
Rahul Rajput <rajputrahul3424@gmail.com>
2025-06-22 21:16:37 -0400

    feat: implement sendEmail function for email service integration

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,8 +1,38 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
 import config from "@/lib/config";
+import emailjs from "@emailjs/browser";
 
 // passing envs for client
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+interface SendEmailArgs {
+  email: string;
+  subject: string;
+  message: string;
+  name?: string;
+}
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+  name = email.split("@")[0],
+}: SendEmailArgs) => {
+  const serviceId = config.env.emailJs.serviceId;
+  const templateId = config.env.emailJs.templateId;
+  const publicKey = config.env.emailJs.publicKey;
+
+  // Map fields to the placeholders used in your EmailJS template.
+  const params = {
+    user_name: name,
+    user_email: email,
+    subject,
+    message,
+  };
+
+  // send the email
+  await emailjs.send(serviceId, templateId, params, { publicKey });
+};
