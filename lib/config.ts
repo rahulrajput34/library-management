09a9a548bd394619db054621927a6f2338299130
Rahul Rajput <rajputrahul3424@gmail.com>
2025-06-24 22:25:52 -0400

    chore: remove emailJs configuration from config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -14,11 +14,7 @@ const config = {
       qstashUrl: process.env.QSTASH_URL!,
       qstashToken: process.env.QSTASH_TOKEN!,
     },
-    emailJs: {
-      serviceId: process.env.EMAILJS_SERVICE_ID!,
-      templateId: process.env.EMAILJS_TEMPLATE_ID!,
-      publicKey: process.env.EMAILJS_PUBLIC_KEY!,
-    },
+    resendToken: process.env.NEXT_PUBLIC_RESEND_TOKEN!,
   },
 };
 
