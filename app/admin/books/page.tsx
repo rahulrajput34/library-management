825d1bdf360928cd8ac3e57096b4572039284f3a
Rahul Rajput <rajputrahul3424@gmail.com>
2025-06-29 23:11:16 -0400

    fix: update button background color for consistency in the admin books page

--- app/admin/books/page.tsx
+++ app/admin/books/page.tsx
@@ -8,7 +8,7 @@ const Page = () => {
     <section className="w-full rounded-2xl bg-white p-7">
       <div className="flex flex-wrap items-center justify-between gap-2">
         <h2 className="text-xl font-semibold">All Books</h2>
-        <Button className="bg-primary-admin" asChild>
+        <Button className="bg-blue-900" asChild>
           <Link href="/admin/books/new" className="text-white">
             + Create a New Book
           </Link>
