40c7f790861541d7783ce6d9d8677f0a6a69bfa7
Rahul Rajput <rajputrahul3424@gmail.com>
2025-07-03 23:02:34 -0400

    fix: update Header component links and add user first name display

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { cn, getInitials } from "@/lib/utils";
+import { cn, getInitials, getFirstName } from "@/lib/utils";
 import { Session } from "next-auth";
 import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
+import { Icon } from "@iconify/react";
 
 const Header = ({ session }: { session: Session }) => {
   const pathname = usePathname();
@@ -30,16 +31,45 @@ const Header = ({ session }: { session: Session }) => {
               pathname === "/library" ? "text-gray-100" : "text-gray-400"
             )}
           >
-            All Books
+            Home
+          </Link>
+        </nav>
+        <nav>
+          <Link
+            href="/"
+            className={cn(
+              "text-sm font-medium transition-colors hover:text-gray-100",
+              pathname === "/library" ? "text-gray-100" : "text-gray-400"
+            )}
+          >
+            Search
           </Link>
         </nav>
         <nav>
           <Link href="/my-profile" className="text-sm font-medium">
             <Avatar>
-              <AvatarFallback className="bg-amber-100 border-2 border-blue-500 rounded-4xl p-2 text-gray-950">
+              <AvatarFallback className="bg-amber-100 border-2 border-blue-500 rounded-4xl p-1 text-gray-950">
                 {getInitials(session.user?.name ?? "")}
               </AvatarFallback>
             </Avatar>
+            <span className="text-sm font-medium ml-1 text-gray-100">
+              {getFirstName(session.user?.name ?? "")}
+            </span>
+          </Link>
+        </nav>
+        <nav>
+          <Link
+            href="/"
+            className={cn(
+              "text-sm font-medium transition-colors hover:text-red-700",
+              pathname === "/library" ? "text-red-800" : "text-red-800"
+            )}
+          >
+            <Icon
+              icon="material-symbols:logout-rounded"
+              width="24"
+              height="24"
+            />
           </Link>
         </nav>
       </div>
