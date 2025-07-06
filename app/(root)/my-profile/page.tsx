cf625f78e42868eab55f25cb472dfd8ad88f1abb
Rahul Rajput <rajputrahul3424@gmail.com>
2025-07-05 23:01:14 -0400

    fix: refactor MyProfilePage component for improved structure and clarity

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import { auth } from "@/auth";
+import { db } from "@/database/drizzle";
+import { users, books, borrowRecords } from "@/database/schema";
+import { eq, asc } from "drizzle-orm";
+import BookList from "@/components/BookList";
+import ProfileForm from "@/components/ProfileForm";
 import { Button } from "@/components/ui/button";
 import { signOut } from "@/auth";
-import BookList from "@/components/BookList";
-import { sampleBooks } from "@/constants";
-import BookOverview from "@/components/BookOverview";
-import { db } from "@/database/drizzle";
-import { books, borrowRecords, users } from "@/database/schema";
-import { asc, eq } from "drizzle-orm";
-import { borrowBook } from "@/lib/actions/book";
 
-const Page = async () => {
-  // get 10 borrowed books
+export default async function MyProfilePage() {
+  // session from imported auth
+  const session = await auth();
+  if (!session?.user?.id) {
+    return <p>Unauthorized</p>;
+  }
+
+  // get my details
+  const [me] = await db
+    .select({
+      fullName: users.fullName,
+      email: users.email,
+      universityId: users.universityId,
+      universityCard: users.universityCard,
+    })
+    .from(users)
+    .where(eq(users.id, session.user.id));
+
+  // borrowed books of that user
   const borrowedBooks = await db
     .select({
       id: books.id,
@@ -34,20 +49,25 @@ const Page = async () => {
     .limit(10);
 
   return (
-    <>
-      <form
-        action={async () => {
-          "use server";
+    <main className="p-8 space-y-12">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold">My profile</h1>
+        {/* Sign-out button  */}
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <Button>Logout</Button>
+        </form>
+      </div>
 
-          await signOut();
-        }}
-        className="mb-10"
-      >
-        <Button>Logout</Button>
-      </form>
+      {/* Editable profile form*/}
+      <ProfileForm initial={me} />
 
-      <BookList title="Borrowed Books" books={borrowedBooks} />
-    </>
+      {/* Borrowed books */}
+      <BookList title="Borrowed books" books={borrowedBooks} />
+    </main>
   );
-};
-export default Page;
+}
