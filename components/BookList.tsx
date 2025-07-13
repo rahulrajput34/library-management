e6c2b476c399222502c21a4638767ab7b1b26670
Rahul Rajput <rajputrahul3424@gmail.com>
2025-07-13 19:57:59 -0400

    feat: adding for LoanedBook functionality

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -3,22 +3,20 @@ import BookCard from "./BookCard";
 
 interface Props {
   title: string;
-  books: Book[];
-  containerClassName?: string;
+  books: LoanedBook[];
+  loaned?: boolean;
 }
 
-const BookList = ({ title, books, containerClassName }: Props) => {
+const BookList = ({ title, books }: Props) => {
   // if the user has less than 1 book, don't show anything
-  if (books.length < 1) return null;
+  if (books.length === 0) return null;
 
   return (
-    <section className={containerClassName}>
-      <h2 className="mb-6 font-mono text-3xl font-semibold tracking-tight">
-        {title}
-      </h2>
-      <ul className="grid gap-y-16 gap-x-12 [grid-template-columns:repeat(auto-fill,minmax(160px,1fr))] place-items-start">
+    <section>
+      <h2 className="mb-6 font-mono text-3xl font-semibold">{title}</h2>
+      <ul className="grid gap-12 [grid-template-columns:repeat(auto-fill,minmax(160px,1fr))]">
         {books.map((book) => (
-          <BookCard key={book.id ?? book.title} {...book} />
+          <BookCard key={book.id} {...book} isLoanedBook />
         ))}
       </ul>
     </section>
