54ad8ead8084461c81fff5fc8b7fd7f7075f3a52
Rahul Rajput <rajputrahul3424@gmail.com>
2025-07-13 21:16:39 -0400

    feat: add a section for  the similar book and make ui little bit better

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -1,55 +1,74 @@
 import React from "react";
-import { db } from "@/database/drizzle";
-import { books } from "@/database/schema";
-import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 import { auth } from "@/auth";
+import { db } from "@/database/drizzle";
+import { books } from "@/database/schema";
+import { and, eq, ne } from "drizzle-orm";
 import BookOverview from "@/components/BookOverview";
 import BookVideo from "@/components/BookVideo";
+import BookCard from "@/components/BookCard";
 
-// params is a promise used for fetching the id from the url
+// page for a selected book
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
-  // Get the id and convert it to a string
+  // current book
   const id = (await params).id;
-
-  // Get the user session
   const session = await auth();
 
-  // Fetch data based on id
+  // current book details
   const [bookDetails] = await db
     .select()
     .from(books)
     .where(eq(books.id, id))
     .limit(1);
 
-  // If the book does not exist, redirect to 404 page
+  // current book details not found
   if (!bookDetails) redirect("/404");
 
+  // genre-based recommendations
+  const similarBooks = await db
+    .select()
+    .from(books)
+    .where(and(eq(books.genre, bookDetails.genre), ne(books.id, id)))
+    .limit(6);
+
   return (
-    <>
-      {/* Book overview section */}
+    <main className="min-h-screen px-4 pb-28 text-gray-100 lg:px-8">
+      {/* book overview */}
       <BookOverview {...bookDetails} userId={session?.user?.id as string} />
 
-      {/* Video and summary section */}
-      <div className="lg:mt-36 mt-16 mb-20 flex flex-col gap-16 lg:flex-row">
-        <div className="flex-[1.5]">
-          <section className="flex flex-col gap-7">
-            <h3>Video</h3>
-
+      {/* video + summary  |||  similar-books */}
+      <div className="mt-20 grid gap-16 lg:grid-cols-[1.5fr_1fr]">
+        {/* left column */}
+        <section className="space-y-16">
+          {/* video */}
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">Video</h3>
             <BookVideo videoUrl={bookDetails.videoUrl} />
-          </section>
-          <section className="mt-10 flex flex-col gap-7">
-            <h3 className="text-xl font-semibold text-blue-950">Summary</h3>
-
-            <div className="space-y-5 text-xl text-gray-100">
+          </div>
+          {/* summary */}
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">Summary</h3>
+            <div className="space-y-5 text-lg leading-relaxed">
               {bookDetails.summary.split("\n").map((line, i) => (
                 <p key={i}>{line}</p>
               ))}
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
+
+        {/* right column */}
+        {/* similar-books */}
+        <aside>
+          <h3 className="mb-6 text-2xl font-semibold">More similar books</h3>
+          <ul className="grid gap-8 [grid-template-columns:repeat(auto-fill,minmax(120px,1fr))]">
+            {similarBooks.map((b) => (
+              <BookCard key={b.id} {...b} />
+            ))}
+          </ul>
+        </aside>
       </div>
-    </>
+    </main>
   );
 };
+
 export default Page;
