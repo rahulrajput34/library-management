501bec93cf6f68c620ed57c51ab5af6920b3b7f9
Rahul Rajput <rajputrahul3424@gmail.com>
2025-07-23 21:22:56 -0400

    fix: add the page for the books for admin side

--- app/admin/books/page.tsx
+++ app/admin/books/page.tsx
@@ -1,25 +1,24 @@
-import React from "react";
+import { listBooks } from "@/lib/admin/actions/book";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import BooksTable from "@/components/admin/BooksTable";
 
-const Page = () => {
+export default async function BooksAdminPage() {
+  const books = await listBooks();
+
   return (
-    // page for creating new books
     <section className="w-full rounded-2xl bg-white p-7">
       <div className="flex flex-wrap items-center justify-between gap-2">
         <h2 className="text-xl font-semibold">All Books</h2>
-        <Button className="bg-blue-900" asChild>
-          <Link href="/admin/books/new" className="text-white">
-            + Create a New Book
-          </Link>
+
+        <Button asChild className="bg-blue-900 text-white">
+          <Link href="/admin/books/new">+ Create a New Book</Link>
         </Button>
       </div>
 
-      <div className="mt-7 w-full overflow-hidden">
-        <p>Table</p>
+      <div className="mt-7 w-full overflow-x-auto">
+        <BooksTable books={books} />
       </div>
     </section>
   );
-};
-
-export default Page;
+}
