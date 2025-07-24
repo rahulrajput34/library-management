64ba0ed7b8c88c398e9d0e9d4cd609da560a219d
Rahul Rajput <rajputrahul3424@gmail.com>
2025-07-23 21:25:46 -0400

    fix: add the update the helpers for the edit the book and displaying the book functionalities

--- lib/admin/actions/book.ts
+++ lib/admin/actions/book.ts
@@ -2,6 +2,25 @@
 
 import { books } from "@/database/schema";
 import { db } from "@/database/drizzle";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import { bookSchema } from "@/lib/validation";
+import { desc } from "drizzle-orm";
+
+// book list
+export async function listBooks() {
+  return db.select().from(books).orderBy(desc(books.createdAt)).limit(10);
+}
+
+// get book by id
+export async function getBookById(id: string) {
+  return db
+    .select()
+    .from(books)
+    .where(eq(books.id, id))
+    .limit(1)
+    .then((res) => res[0]);
+}
 
 // create book with data we get it from the from
 export const createBook = async (params: BookParams) => {
@@ -29,3 +48,22 @@ export const createBook = async (params: BookParams) => {
     };
   }
 };
+
+export async function updateBook(
+  id: string,
+  values: z.infer<typeof bookSchema>
+) {
+  const parsed = bookSchema.safeParse(values);
+  if (!parsed.success) {
+    return { success: false, message: "Invalid data." };
+  }
+
+  await db.update(books).set(parsed.data).where(eq(books.id, id));
+
+  return { success: true, message: "Book updated.", data: { id }};
+}
+
+export async function deleteBook(id: string) {
+  await db.delete(books).where(eq(books.id, id));
+  return { success: true };
+}
