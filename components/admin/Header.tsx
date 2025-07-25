d76f4c8a9312aded2e120bfbafcb1376e621b64f
Rahul Rajput <rajputrahul3424@gmail.com>
2025-07-25 17:30:23 -0400

    fix: change the content of the text to make it workable across all other pages

--- components/admin/Header.tsx
+++ components/admin/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({ session }: { session: Session }) => {
           {session?.user?.name}
         </h2>
         <p className="text-base text-slate-500">
-          Monitor all of your users and books here
+          Monitor all of your projects and tasks here
         </p>
       </div>
     </header>
